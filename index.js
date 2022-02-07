3fcce4d3ab779dff0bedca58186296d941a7fd3b
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-07 17:19:56 -0300

    c1- adding index and routes

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const api = express()
+const port = 3000
+
+const movies = require('./routes/series')
+api.use('/series', movies)
+
+//const series = [{
+    //name:'frinds',
+    //nametwo: 'homem de ferro',
+  //  nametree:'Wolverine'
+//}]
+//api.get('/series', (req,res) => {
+  //  res.send(series)
+//})
+
+
+api.listen(port, ()=> console.log('starting serving'))
