6f5c3c4d5ec5a7cced9c8b1a32c1d31677c04477
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-09 17:04:42 -0300

    c23- deleted index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const api = express()
 const port = process.env.PORT || 3000
+const organize = require('./lib/organize')
 
 const mongo = process.env.MONGO ||  'mongodb://localhost/minhas-series-rest'
 const mongoose = require('mongoose')
@@ -10,17 +11,24 @@ const bodyParser = require('body-parser')
 api.use(bodyParser({extended: true}))
 
 const path = require('path')
+
 api.set('view engine','ejs')
 api.set('views', path.join(__dirname,'views'))
 
-api.set(express.static(path.join(__dirname,'public')))
+api.use(express.static(path.join(__dirname,'public')))
 
 
 const movies = require('./routes/series')
 api.use('/series', movies)
 
+const teste = require('./routes/series')
+api.use('/',teste)
 
+api.get('/', (req, res) =>{
+    
+  res.render('home')
 
+})
 
 mongoose.
 connect(mongo, {useNewUrlParser: true, 
