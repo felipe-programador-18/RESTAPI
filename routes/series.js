f846d410694e4b1184d0d9a7a417dda5eb5ca8fc
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-09 13:25:18 -0300

    c17- adding new modify

--- routes/series.js
+++ routes/series.js
@@ -2,61 +2,47 @@ const express = require('express')
 const router = express.Router()
 const Serie = require('../models/serie')
 
-const series = [{
-    name:'frinds',
-    nametwo: 'homem de ferro',
-    nametree:'Wolverine'
-}]
-// create router to adding get
-router.get('/', async(req,res) =>{
+router.get('/', async (req, res) => {
     const series = await Serie.find({})
-    res.send([series])
-})
-
-
-//create router to get post
-router.post('/', async (req,res) => {
-   const serie  = new Serie(req.body)
-   try{
-       await serie.save()
-       res.send(serie)
-   }catch(e){
-       res.send({
-           success: false,
-           errors: Object.keys(e.errors)
-       })
-   }
-})
-
-//adding router deleted
-
-router.delete('/:id', async(req, res) => {
-  await Serie.remove({id: req.params.id})
-  res.send({
-      success:true
-    })
-})
-
-router.get('/:id', async (req, res) =>{
-    const serie = await Serie.findOne({_id: req.params.id})
-    res.send(serie)
-} )
-
-router.put('/:id', async (req,res) => {
- const serie = Serie.findOne({_id: req.params.id})
- serie.name = req.body.name
- serie.status = req.body.status
-
- try{
-     await serie.save()
-     res.send(serie)
- } catch(e){
-    res.send({
+    res.send(series)
+  })
+  router.post('/', async (req, res) => {
+    const serie = new Serie(req.body)
+    try {
+      await serie.save()
+      res.send(serie)
+    } catch (e) {
+      res.send({
         success: false,
         errors: Object.keys(e.errors)
+      })
+    }
+  })
+  router.delete('/:id', async (req, res) => {
+    await Serie.remove({ _id: req.params.id })
+    res.send({
+      success: true
     })
- }
-})
-
+  })
+  
+  router.get('/:id', async (req, res) => {
+    const serie = await Serie.findOne({ _id: req.params.id })
+    res.send(serie)
+  })
+  
+  router.put('/:id', async (req, res) => {
+    const serie = await Serie.findOne({ _id: req.params.id })
+    serie.name = req.body.name
+    serie.status = req.body.status
+    try {
+      await serie.save()
+      res.send(serie)
+    } catch (e) {
+      res.send({
+        success: false,
+        errors: Object.keys(e.errors)
+      })
+    }
+  })
 
 module.exports = router
